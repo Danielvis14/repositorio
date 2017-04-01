91f9a702cfbd38e7e18521a8ef2a87ce50dfaf40
Danielvis14 <danielgamerbr14@gmail.com>
2017-04-01 14:26:41 -0300

    Update exercicio3.sh

--- exercicio3.sh
+++ exercicio3.sh
@@ -8,9 +8,8 @@ tx=$(awk '{if($1<=0) print "0";else if($1<=500) print "20";else if($1>500) print
     if ((tx==0));then 
     exit
     else
-    #Calculo com o awk
-    echo -e "\n O salário: $sal foi acrescido de ${tx}%\n Salário Corrigido: $(awk '{printf "%.2f" ,$1*($2/100+1)}' <<< "$sal $tx"|sed 's/\./,/')"
-    #Calculo com o bc
+
+     #Calculo com o bc
      echo -e "\n O salário: $sal foi acrescido de ${tx}%\n Salário Corrigido: $(bc <<< "scale=2; (${tx}/100+1) * $sal" | sed 's/\./,/')"
-    CALCULA_SALARIO
+    
     fi
